05a3ae1b1950d8d8fbaecd64720f98bd6365ebe8
srji <iosergio.valbuena@gmail.com>
2022-04-11 13:07:00 -0700

    starting monday

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,66 @@
 
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementById("emptyCart").addEventListener('click', emptyCart)
+}
+
+function emptyCart(){
+    var cartItems = document.getElementById('cart-items')
+    while(cartItems.hasChildNodes()){
+        cartItems.removeChild(cartItems.firstChild);
+    }
+    updateCartTotal();
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    addItemToCart(title, price)
+    updateCartTotal()
+}
+
+
+
+
 
 const fetchData = (url_api) => {
     return new Promise((resolve, reject) => {
@@ -60,9 +122,9 @@ window.fetch(API)
             addToCard.className = "btn btn-outline-success flex-row-reverse m-2"
             addToCard.id = "addToCard";
             addToCard.addEventListener('click', function(){
-                console.log(title.textContent);
-                console.log(price.textContent);
-                console.log(category.textContent);
+                //console.log(title.textContent);
+                //console.log(price.textContent);
+                //console.log(category.textContent);
                 addItemToCart(title.textContent, price.textContent)
                 
             });
@@ -92,7 +154,6 @@ window.fetch(API)
 var viewCart = document.getElementById('viewCart');
 viewCart.addEventListener('click', updateCartTotal)
 
-
 function updateCartTotal() {
         var cartItemContainer = document.getElementsByClassName('cart-items')[0]
         var cartRows = cartItemContainer.getElementsByClassName('cart-row')
@@ -109,25 +170,23 @@ function updateCartTotal() {
         document.getElementsByClassName('cart-total-price')[0].innerText = formatPrice(total);
     }
 
-
-
-
 function addItemToCart(title, price) {
         var cartRow = document.createElement('div')
         cartRow.classList.add('cart-row')
         var cartItems = document.getElementsByClassName('cart-items')[0]
         var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+  
         for (var i = 0; i < cartItemNames.length; i++) {
-            if (cartItemNames[i].innerText == title) {
+            if (cartItemNames.innerText == title) {
                 alert('This item is already added to the cart')
-                return
+                
             }
         }
+
         var cartRowContents = `
             
+        <span class="cart-item-title">${title}</span>
             <div class="cart-item cart-column">
-                
-                <span class="cart-item-title">${title}</span>
             </div>
             <div class="cart-column">
             <input class="cart-quantity-input" type="number" value="1">
@@ -144,6 +203,13 @@ function addItemToCart(title, price) {
     }
 
 
+const APIcurrency = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/';
+
+window.fetch(APIcurrency)
+    .then((response) => response.json())
+    .then((responseCurrency)=>{
+        console.log(responseCurrency);
+    })
 
 
 
@@ -202,71 +268,39 @@ const formatPrice = (price) => {
 const anotherFunction = async (url_api) => {
     try{
         const data = await fetchData(url_api)
-        console.log(data);
-        for (let i = 0; i < data.length; i++) {
-            const title = data[i].title;
-            const price = data[i].price;
-            const description = data[i].description;
-            const image = data[i].image;
-            const category = data[i].category;
-            //console.log(title);
-            app2.innerHTML = `
-                <div class="col">
-                    <div class="card h-100">
-                    <img src="${image}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title" id="itemTitle">${title}</h5>
-                            <p class="card-text">${description}</p>
-                        </div>
-                        <div class=" d-flex">
-                            <b class="nav me-auto ms-3 mt-3" id="itemPrice">$ ${price}</b>
-                            <button class="btn btn-outline-success flex-row-reverse m-2" id="addButton">Add to Cart</button>
-                        </div>
-                    </div>
-                </div>
-            `
-        }
+        console.log(data[0]);
+        // for (let i = 0; i < data.length; i++) {
+        //     const title = data[i].title;
+        //     const price = data[i].price;
+        //     const description = data[i].description;
+        //     const image = data[i].image;
+        //     const category = data[i].category;
+        //     console.log(title);
+        //     document.getElementById("currencySelected").innerHTML = `
+        //         <div class="col">
+        //             <div class="card h-100">
+        //             <img src="${image}" class="card-img-top" alt="...">
+        //                 <div class="card-body">
+        //                     <h5 class="card-title" id="itemTitle">${title}</h5>
+        //                     <p class="card-text">${description}</p>
+        //                 </div>
+        //                 <div class=" d-flex">
+        //                     <b class="nav me-auto ms-3 mt-3" id="itemPrice">$ ${price}</b>
+        //                     <button class="btn btn-outline-success flex-row-reverse m-2" id="addButton">Add to Cart</button>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     `
+        // }
     }catch(error){
         console.log(`este es el error: ${error}`);
     }
-    
 
-    //Add cart Button
-    // var addButton = document.getElementById("addButton");
-    // addButton.addEventListener("click", function(){
-    //     console.log("Add to Cart Button clicked");
-    //     document.getElementById('shoppingList').innerHTML = `
-    //     <li class="list-group-item d-flex justify-content-between lh-sm">
-    //     <div>
-    //       <h6 class="my-0 r-4">Product name</h6>
-    //       <small class="text-muted">Brief de description</small>
-    //       <br>
-
-    //       <span class="text-danger" id="deleteButton"> 
-    //         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
-    //           <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
-    //         </svg>
-    //         Delete</span>
-    //     </div>
-    //     <span class="text-muted">$12</span>
-    //   </li>
-
-    //     `;
-        
-
-    //                 //Delete item form cart 
-    //         var deleteButton = document.getElementById("deleteButton");
-    //         deleteButton.addEventListener('click', function(event){
-    //             //console.log("clck on deleteButton");
-    //             var deleteClicked = event.target
-    //             deleteClicked.parentElement.parentElement.parentElement.remove() 
-    //         })
-    // })
 
 
 }
 
-//anotherFunction(API);
+anotherFunction(APIcurrency);
 
 
 
